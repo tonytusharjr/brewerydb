88e2efb929b2968e6e14cb18e76e0c17d8aaee6c
Tony Tushar <tonytusharjr@gmail.com>
2020-08-14 13:04:53 -0500

    Add setup.py file

--- brewerydb.py
+++ brewerydb.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import requests
 
 DEFAULT_BASE_URI = "http://api.brewerydb.com/v2"
@@ -12,6 +15,7 @@ simple_endpoints = ["beers", "breweries", "categories", "events",
 single_param_endpoints = ["beer", "brewery", "category", "event",
                           "feature", "glass", "guild", "ingredient",
                           "location", "socialsite", "style", "menu"]
+
 
 class BreweryDb:
 
@@ -31,7 +35,7 @@ class BreweryDb:
 
     @staticmethod
     def _get(request, options):
-        options.update({"key" : BreweryDb.API_KEY})
+        options.update({"key": BreweryDb.API_KEY})
         return requests.get(BreweryDb.BASE_URI + request, params=options).json()
 
     @staticmethod
